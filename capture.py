2e3e1b34e5dadcdd19ea929e83ae329b76d4361f
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-04-12 15:55:23 +0530

    ADD function to obtain names of all the markets on Binance

---
+++ capture.py
@@ -0,0 +1,45 @@
+import json
+import requests
+import websocket
+
+
+# The base endpoint for the Binance RESTful API
+BASE_REST = "https://api.binance.com"
+
+# API GET endpoints to perform several checks
+CONN_TEST = "/api/v3/ping"
+SERVER_TIME = "/api/v3/time"
+
+# Additional End Points to retrieve information
+MARKET_PRICE = "/api/v3/ticker/price"
+DATA_24HR = "/api/v3/ticker/24hr"
+
+
+# Global list for all the available markets
+MARKETS = None
+
+
+
+
+
+def get_market_list():
+  """
+  To get the list of all the available markets
+  Returns:
+    <list> of all the market names
+  """
+  global MARKETS
+
+  # GET the market price data for all the listed symbols on Binance
+  URL = BASE_REST + MARKET_PRICE
+  response = requests.get(URL)
+  markets_price_list = json.loads(response.text)
+
+  # Use the price list to obtains names of all the markets
+  market_names = []
+  for record in markets_price_list:
+    name = record['symbol']
+    market_names.append(name)
+
+  # Populate the global variable
+  MARKETS = market_names

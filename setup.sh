d1089db46a75fd369b5b7ad0e79fe5dc60057445
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-04-13 14:03:24 +0530

    Add script to setup systems for running the application

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo pt update
+sudo apt upgrade
+sudo apt install python3-pip
+pip install --user -r requirements.txt
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/os-release
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt-get update && sudo apt-get install influxdb
+sudo service influxdb start

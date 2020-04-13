9ac249c0634d00eee68f2c05903a6400cacddc70
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-04-13 15:31:11 +0530

    Change pip->pip3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo pt update
 sudo apt upgrade
 sudo apt install python3-pip
-pip install --user -r requirements.txt
+pip3 install --user -r requirements.txt
 wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/os-release
 echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list

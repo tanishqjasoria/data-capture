4be311e9e6623e1e580c8ca2a7461aee326538f8
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-04-14 23:35:55 +0530

    Database initialization methods and variables

--- check_data_quality.py
+++ check_data_quality.py
@@ -1,4 +1,25 @@
 import argparse
+import binance_data
+
+from influxdb import InfluxDBClient
+
+
+# Default HOST and PORT for the instance of InfluxDB
+HOST = 'localhost'
+PORT = 8086
+
+# Name of the database to be checked, initialized thorough command lin args.
+DATABASE = None
+
+# Various measurements to be used for storing various ticker data
+MEASUREMENT_1M = 'CRYPTO_1M'
+MEASUREMENT_5M = 'CRYPTO_5M'
+MEASUREMENT_15M = 'CRYPTO_15M'
+MEASUREMENT_30M = 'CRYPTO_30M'
+MEASUREMENT_1H = 'CRYPTO_1H'
+
+# Query to get the count of the number of records for each Market for each measurement.
+COUNT_DATA_QUERY = "SELECT COUNT(\"High\") FROM " + DATABASE + "..{} GROUP BY \"Market\""
 
 
 
@@ -22,5 +43,23 @@ def parse_arguments():
   args = vars(ap.parse_args())
 
   return args
+
+
+
+def database_setup():
+  """
+  Initialize the database for quality check
+  Returns:
+    <InfluxDBClient>
+  """
+
+  # Initialise the InfluxDB client
+  client = InfluxDBClient(HOST, PORT)
+
+  # Create the required database, if the databse is already present
+  # this would just return without any exception
+  client.create_database(DATABASE)
+  client.switch_database(DATABASE)
+  return client
 
 

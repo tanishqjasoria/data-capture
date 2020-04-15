8b728e8f0fbe6cda1d105dfaf04589f0c9f4f981
Tanishq Jasoria <jasoriatanishq@gmail.com>
2020-04-15 19:54:57 +0530

    FIX python->python3

--- crypto_store.py
+++ crypto_store.py
@@ -163,11 +163,13 @@ def setup_run_testing_module():
   Start the 'check_data_quality at regular intervals
   """
 
+  time.sleep(3600 * INTERVAL)
   proc = subprocess.Popen(
-      ["python", "check_data_quality.py", "-DB"," crypto_ticker", "-C", str(COUNT)],
+      ["python3", "check_data_quality.py", "-DB"," crypto_ticker", "-C", str(COUNT)],
       stdout=subprocess.PIPE,
       stderr=subprocess.PIPE)
-  time.sleep(3600 * INTERVAL)
+  logger.error(proc.communicate())
+
 
 
 
